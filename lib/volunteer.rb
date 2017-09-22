7e1c21fd37371f08e6d474ddde191ab8c784651a
Safiy Cham <csafi13@yahoo.com>
2017-09-22 10:39:28 -0700

    add passing spec test to return the same volunteer if two volunteers have the same name.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -6,4 +6,8 @@ class Volunteer
     @id = attributes.fetch(:id)
     @project_id = attributes.fetch(:project_id)
   end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name()).&(self.project_id().== (another_volunteer.project_id()))
+  end
 end
