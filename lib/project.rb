38ae6dc4b498fb36f6cc6246ed423f53c7007e5c
Safiy Cham <csafi13@yahoo.com>
2017-09-22 09:45:20 -0700

    add passing spec test to return a project by id.

--- lib/project.rb
+++ lib/project.rb
@@ -25,4 +25,14 @@ class Project
     end
     projects
   end
+
+  def self.find(id)
+    found_project = nil
+    Project.all().each() do |project|
+      if project.id().==(id)
+        found_project = project
+      end
+    end
+    found_project
+  end
 end
